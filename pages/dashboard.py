77b0544d07af3d9a94974e32be61cc2b475996e9
Anishmayadav <rsi2024505@iiita.ac.in>
2025-11-16 21:56:46 +0530

    Update dashboard.py

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -3,6 +3,4 @@ import streamlit as st
 st.title("Dashboard")
 
 st.page_link("pages/color1.py", label="Color Tracker 1")
-st.page_link("pages/color2.py", label="Color Tracker 2")
-st.page_link("pages/color3.py", label="Color Tracker 3")
-st.page_link("pages/color4.py", label="Color Tracker 4")
+
